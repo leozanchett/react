320243ff5115b2b959cc414615ca62c6718cab5f
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-27 15:44:34 -0300

    map()

---
+++ material/advanced-jsx/src/map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <ul>{peopleLis}</ul>,
+  document.getElementById('root')
+)
