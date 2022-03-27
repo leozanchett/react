318419a2df2b616df085642107b8a19e8030e3d9
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-27 15:30:53 -0300

    Event Listeners in JSX

--- material/advanced-jsx/src/index.js
+++ material/advanced-jsx/src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
 
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  if(e.target.alt === 'kitty'){
+    e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+    e.target.setAttribute('alt', 'doggy');
+  } else {
+    e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg');
+    e.target.setAttribute('alt', 'kitty');
+  }
+  
+}
 
-const myDiv = (
-  <div className="big">
-     I AM A BIG DIV
-  </div>
+const kitty = (
+	<img 
+        onClick={makeDoggy}
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty"
+        width="500"
+        height="400"
+    />
 );
 
-ReactDOM.render(
-  myDiv ,
-  document.getElementById('root')
-);
-
+ReactDOM.render(kitty, document.getElementById('root'));
