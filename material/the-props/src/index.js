812e3790166e314d6748b3001450e159fb47e9c5
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-28 08:17:35 -0300

    props

---
+++ material/the-props/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import { PropsDisplayer } from './propsDisplayer';
+;
+
+ReactDOM.render(
+  <PropsDisplayer myProp='Hello'/>,
+  document.getElementById('app')
+);
+
+
