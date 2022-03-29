bc738883603e62a2fe3f9ff7bb3b706ce973401e
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-29 17:07:21 -0300

    add thought funcionality

--- projects/passing-thoughts/src/AddThoughtForm.js
+++ projects/passing-thoughts/src/AddThoughtForm.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
+  const [thought, setThought] = useState('');
+
+  const handleTextChange = ({ target }) => {
+    setThought(target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (thought) {
+      props.addThought({
+        id: generateId(),
+        text: thought,
+        expiresAt: getNewExpirationTime(),
+      });
+      setThought('');
+    }
+  }
+
   return (
-    <form className="AddThoughtForm">
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
       <input
+        value={thought}
+        onChange={handleTextChange}
         type="text"
         aria-label="What's on your mind?"
         placeholder="What's on your mind?"
