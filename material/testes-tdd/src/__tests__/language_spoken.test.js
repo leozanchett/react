73880e90622c534f7ee5a3f451708a74215f1dff
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-30 11:30:06 -0300

    Testes react

---
+++ material/testes-tdd/src/__tests__/language_spoken.test.js
@@ -0,0 +1,28 @@
+import {
+  capitalize,
+  getAlpha2Code,
+  countryExtractor,
+  countryListLookup,
+  getResponse
+} from "../language_spoken.js";
+
+test("convert array of country data objects to array of countries", ()=>{
+    //arrange
+    const inputObject = [
+      {name: "Argentina", capital: "Buenos Aires"},
+      {name: "Belize", capital: "Belmopan"},
+      {name: "Bolivia", capital: "Sucre"}
+      ]
+    const expectedValue = ["Argentina","Belize","Bolivia"]
+    
+    //act
+    const actualValue = countryExtractor(inputObject)
+    
+    //assertions
+
+       expect(actualValue).toEqual(expectedValue)
+       expect(actualValue[0]).toBe("Argentina")
+       expect(actualValue).toContain("Belize")
+       expect(actualValue[2] === "Bolivia").toBeTruthy()
+       expect(actualValue[3]).not.toBeDefined()
+})
